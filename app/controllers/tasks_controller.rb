0b1642ee7d9ed678d2e27d393c8a6c8155b50fee
Aseiide <eiseiide@gmail.com>
2021-10-11 09:52:00 +0900

    登録アクションを実装した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,17 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。" 
+  end
+
   def edit
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
