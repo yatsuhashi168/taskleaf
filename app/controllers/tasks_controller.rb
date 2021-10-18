ecb7de011c37e877f8ecebab63481d54ca3af120
yuma <ikeoku.y@gmail.com>
2021-10-18 09:40:02 +0900

    削除機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,12 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :description)
