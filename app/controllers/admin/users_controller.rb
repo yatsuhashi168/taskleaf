0a71d1f22be59b0fae46209cb769d4a2ccb4b064
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2021-10-26 09:46:03 +0900

    ユーザー登録のviewとcontrollerを作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,21 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+    end
   end
 
-  def edit
-  end
-
-  def show
-  end
-
-  def index
-  end
 end
