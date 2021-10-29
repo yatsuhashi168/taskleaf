a45da4c9a925ec5fd7319368b4625d841bbda376
Aseiide <eiseiide@gmail.com>
2021-10-29 09:54:48 +0900

    管理者ユーザーのサンプルデータを定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin_user = User.create!(name: 'admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true)
+
+puts "succeed"
