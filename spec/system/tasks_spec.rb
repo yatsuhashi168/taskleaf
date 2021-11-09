d3d6485b999f9ae5fb1e9036d6059889e698ed7b
Aseiide <eiseiide@gmail.com>
2021-11-09 09:45:26 +0900

    タスクの一覧機能のテストを途中まで追加

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # 作成者がユーザーAであるタスクを作成
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
+
+    context 'ユーザーAがログインしているとき' do
+      before do
+        # ユーザーAでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 作成済みのタスクの名称が画面上に表示されていることを確認
+      end
+    end
+  end
+end
+
