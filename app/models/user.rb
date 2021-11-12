726873f7f456b6969e69f136843eb293b8aa70f5
eatplaynap <eatplaynap329@gmail.com>
2021-11-12 09:34:18 +0900

    rubocopの指示により、タスク作成者が削除されたら紐づくタスクが削除させるよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
