665e027e6b5fa09e6422716134240621f9a61f8c
eatplaynap <eatplaynap329@gmail.com>
2021-11-12 09:47:01 +0900

    詳細表示機能のテストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,20 +3,19 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+
+  before do
+    # ユーザーAでログインする
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
   describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-
-    before do
-      # 作成者がユーザーAであるタスクを作成
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      # ユーザーAでログインする
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
@@ -35,4 +34,19 @@ describe 'タスク管理機能', type: :system do
       end
     end
   end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+  end
 end
+
