0abf4949b1b940541919f69ea67a6ae69cfab1fb
yuma <ikeoku.y@gmail.com>
2021-11-15 09:35:59 +0900

    itの共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -15,14 +15,15 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
