76561b3e08fcdd3d5115a88901df083c3f057f79
Aseiide <eiseiide@gmail.com>
2021-11-16 09:37:12 +0900

    テストを失敗するように変更した

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   belongs_to :user
