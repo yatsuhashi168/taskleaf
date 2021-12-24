fa45ffb87b9f6fe46c7ff8bec4a57123033f26c4
Aseiide <eiseiide@gmail.com>
2021-12-24 09:45:49 +0900

    画像アップロード機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
