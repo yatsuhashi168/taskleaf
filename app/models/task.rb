94501b3a3d9aa1cea8b53c2dd76dafa0ebb497c1
shirotamaki <shiro.tamaki63@gmail.com>
2021-12-27 09:47:21 +0900

    csv形式のファイル出力機能の追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,6 +17,19 @@ class Task < ApplicationRecord
     []
   end
 
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{ |attr| task.send(attr) }
+      end
+    end
+  end
+
   private
 
   def validate_name_not_including_comma
