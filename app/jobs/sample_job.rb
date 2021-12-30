d4e9471c377b2a134d654d1e509e2fd1314b74f7
yuma <ikeoku.y@gmail.com>
2021-12-30 09:51:55 +0900

    rubocop対応

--- app/jobs/sample_job.rb
+++ app/jobs/sample_job.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class SampleJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    Sidekiq::Logging.logger.info "サンプルジョブを実行しました"
+  def perform(*_args)
+    Sidekiq::Logging.logger.info 'サンプルジョブを実行しました'
   end
 end
