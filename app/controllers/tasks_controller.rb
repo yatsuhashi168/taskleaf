355096d424cd59a78a20e219f213d659512ee14c
Aseiide <eiseiide@gmail.com>
2022-01-04 09:52:46 +0900

    非同期処理を外した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,6 @@ class TasksController < ApplicationController
 
     if @task.save
       TaskMailer.creation_email(@task).deliver_now
-      SampleJob.set(wait: 1.week).perform_later
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
