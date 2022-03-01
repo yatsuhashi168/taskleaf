746728fb418033f1649b5f1874e55c4db8b31a6f
Aseiide <eiseiide@gmail.com>
2022-03-01 21:04:47 +0900

    simplecovを追加してカバレッジを見れるようにした。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,4 +99,6 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+  require 'simplecov'
+  SimpleCov.start
 end
